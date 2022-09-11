653a805a7cbc2347874db3d38fd5d33d47796de7
DeukYu <ldy8070@naver.com>
2022-09-12 00:09:11 +0900

    UI Script 작업
    UI Script 작업

--- Client/Assets/Scripts/Managers/Core/UIManager.cs
+++ Client/Assets/Scripts/Managers/Core/UIManager.cs
@@ -2,8 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager
 {
+    int _order = 10;
+
+    public GameObject Root
+    {
+        get
+        {
+            GameObject root = GameObject.Find("@UI_Root");
+            if (root == null)
+                root = new GameObject { name = "@UI_Root" };
+            return root;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
